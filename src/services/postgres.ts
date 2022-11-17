cad0e3dab522a6ea410bf0b4eaec039b968fa8d0
CozyBrian <dragonbrian33@gmail.com>
2022-11-17 13:37:51 +0000

    merge dev

--- src/services/postgres.ts
+++ src/services/postgres.ts
@@ -2,11 +2,8 @@ require('dotenv').config({path:__dirname+'/../.env'})
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'shower_thoughts',
-  password: 'tesla',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
 });
 
 export default pool;
